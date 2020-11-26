a9335e6f8bbd8ac97a34540a2b898c8a442b5dcd
Ramon Ariel Leon Flores <arielleon24@gmail.com>
2020-11-26 13:47:43 +0000

    sanitized teacher.js

--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -1,15 +1,30 @@
-pool.query(`
+const { Pool } = require('pg'); 
+
+const cohortName = process.argv[2];
+const limit = process.argv[3] || 5;
+// Store all potentially malicious values in an array. 
+const values = [`%${cohortName}%`];
+
+const pool = new Pool({
+  user: 'vagrant',
+  password: '123',
+  host: 'localhost',
+  database: 'bootcampx'
+});
+
+const queryString = `
 SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
 FROM teachers
 JOIN assistance_requests ON teacher_id = teachers.id
 JOIN students ON student_id = students.id
 JOIN cohorts ON cohort_id = cohorts.id
-WHERE cohorts.name = '${process.argv[2] || 'JUL02'}'
-ORDER BY teacher;
-`)
-.then(res => {
-  res.rows.forEach(row => {
-    console.log(`${row.cohort}: ${row.teacher}`);
-  })
-});
+WHERE cohorts.name = $1
+ORDER BY teacher
+  `;
 
+pool.query(queryString, values)
+.then(res => {
+  res.rows.forEach(user => {
+    console.log(`${user.name} has an id of ${user.student_id} and was in the ${user.cohort} cohort`);
+  })
+}).catch(err => console.error('query error', err.stack));
